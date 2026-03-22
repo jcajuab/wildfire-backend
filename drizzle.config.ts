deaeb4c78b386cd9f629a930d472ad0b11abd81e
jcajuab <jcajuab@gmail.com>
2026-03-22 19:06:31 +0800

    ci: update drizzle.config.ts to use process.env.* directly

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from "drizzle-kit";
-import { DATABASE_URL, env } from "#/env";
+
+const host = process.env.MYSQL_HOST ?? "localhost";
+const port = Number(process.env.MYSQL_PORT ?? 3306);
+const user = process.env.MYSQL_USER ?? "";
+const password = process.env.MYSQL_PASSWORD ?? "";
+const database = process.env.MYSQL_DATABASE ?? "";
 
 export default defineConfig({
   dialect: "mysql",
   schema: "./src/infrastructure/db/schema/*.sql.ts",
   casing: "snake_case",
   dbCredentials: {
-    host: env.MYSQL_HOST,
-    port: env.MYSQL_PORT,
-    database: env.MYSQL_DATABASE,
-    user: env.MYSQL_USER,
-    password: env.MYSQL_PASSWORD,
-    url: DATABASE_URL,
+    host,
+    port,
+    database,
+    user,
+    password,
+    url: `mysql://${user}:${password}@${host}:${port}/${database}`,
   },
 });
